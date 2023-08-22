074a45916bf4db7d400502674ccd0ea96bec4ca1
mitani1993 <fytim55@gmail.com>
2023-08-22 10:44:47 +0900

    クラス式でクラスを作成する

---
+++ src/chap5/1-8.ts
@@ -0,0 +1,17 @@
+const User = class {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  isAdult(): boolean {
+    return this.age >= 20;
+  }
+};
+
+const uhyo = new User("uhyo", 26);
+console.log(uhyo.name);
+console.log(uhyo.isAdult());
