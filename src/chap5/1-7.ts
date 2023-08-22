725036960b9319d14be9c4b59f4700bb50bc1928
mitani1993 <fytim55@gmail.com>
2023-08-22 10:43:15 +0900

    コンストラクタ引数でのプロパティ宣言

---
+++ src/chap5/1-7.ts
@@ -0,0 +1,11 @@
+class User {
+  constructor(public name: string, private age: number) {}
+
+  isAdult(): boolean {
+    return this.age >= 20;
+  }
+}
+
+const uhyo = new User("uhyo", 26);
+console.log(uhyo.name);
+console.log(uhyo.isAdult());
